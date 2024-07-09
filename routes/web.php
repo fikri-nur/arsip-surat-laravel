09eb116fa5b9be28228d155ad8cdab5efb352aac
fikri-nur <fikriiinur07@gmail.com>
2024-07-10 04:02:12 +0700

    make route for dashboard, mail, and about page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MailController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 /*
@@ -19,4 +21,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard');
+Route::resource('surat', MailController::class);
+Route::get('/kategori-surat', [MailController::class, 'category'])->name('surat.kategori');
+Route::get('/about', [HomeController::class, 'about'])->name('about');
